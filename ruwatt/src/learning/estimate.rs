24a75371ef559e56f6535befd6bb7ad3fa167a81
Evgeny <47105013+evm72011@users.noreply.github.com>
2024-06-23 15:17:51 +0200

    r2_score

--- ruwatt/src/learning/estimate.rs
+++ ruwatt/src/learning/estimate.rs
@@ -1,4 +1,5 @@
 use num::Float;
+use num::traits::FromPrimitive;
 use std::iter::Sum;
 use crate::tensor::{Tensor, Vector};
 use crate::{assert_matrix, assert_shape};
@@ -39,42 +40,41 @@ pub fn mad<T>(y_predict: &Tensor<T>, y_test: &Tensor<T>) -> Tensor<T> where T : 
     }
 }
 
-/*
-pub fn r2_score<T>(y_predict: &Tensor<T>, y_test: &Tensor<T>) -> Tensor<T> where T : Float + Sum {
+pub fn r2_score<T>(y_predict: &Tensor<T>, y_test: &Tensor<T>) -> Tensor<T> where T : Float + Sum<T>{
     assert_shape!(y_predict, y_test);
     assert_matrix!(y_predict);
     let row_count = y_predict.row_count();
     if row_count == 0 {
-        Tensor::empty()
-    } else {
-        let means = y_test.cols()
-            .map(|col| col.data.iter().sum() / col.data.len());
+        return Tensor::empty();
+    } 
+    let row_means: Vec<T> = y_test.cols()
+        .map(|col_test| col_test.data.iter().cloned().sum::<T>() / T::from(col_test.data.len()).unwrap())
+        .collect();
 
-        let ssrs: Vec<T> = y_predict.cols().zip(y_test.cols())
-            .map(|(col_model, col_test)| {
-                let difference = col_model - col_test;
-                let summ: T = difference.data.iter().map(|&value| value.powi(2)).sum();
-                summ / T::from(row_count).unwrap()
-            })
-            .collect();
+    let ss_res: Vec<T> = y_test.cols().zip(y_predict.cols())
+        .map(|(col_test, col_predict)| {
+            let col_diff = col_test - col_predict;
+            let summ: T = col_diff.data.iter().map(|&value| value.powi(2)).sum();
+            summ
+        })
+        .collect();
 
-        let ssrt: Vec<T> = y_test.cols().zip(means)
+    let ss_tot: Vec<T> = y_test.cols().zip(row_means)
             .map(|(col_test, mean)| {
-                let difference = col_test - mean;
-                let summ: T = difference.data.iter().map(|&value| value.powi(2)).sum();
-                summ / T::from(row_count).unwrap()
+                let summ: T = col_test.data.iter().map(|&value| (value - mean).powi(2)).sum();
+                summ
             })
             .collect();
 
-        //let data = 
-        Vector::ket(data)
-    }
+    let data = ss_res.iter().zip(ss_tot.iter())
+            .map(|(&res, &tot)| T::one() - res / tot)
+            .collect();
+    Vector::bra(data)
 }
-*/
 
 #[cfg(test)]
 mod tests {
-    use super::{mse, mad };
+    use super::{mad, mse, r2_score };
     use crate::tensor::{Vector, Matrix};
 
     #[test]
@@ -93,5 +93,13 @@ mod tests {
         let recieved = mad(&y_model, &y_test);
         let expected = Vector::ket(vec![0.5]);
         assert_eq!(recieved, expected)
+    }
+
+    #[test]
+    fn r2_score_test() {
+        let predict = Vector::ket(vec![2.5, 0.0, 2.0, 8.0]);
+        let test = Vector::ket(vec![3.0, -0.5, 2.0, 7.0]);
+        let recieved = r2_score(&predict, &test).to_scalar();
+        assert!(f32::abs(recieved - 0.9484) < 0.001)
     }
 }
