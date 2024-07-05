52604239646a64e0769c67dd1d9f94df36e98f66
eugen.michaels-at-927502379560 <eugen.michaels@hannover-re.com>
2024-07-05 15:18:01 +0200

    not lose first line

--- ruwatt/src/data_frame/data_frame.rs
+++ ruwatt/src/data_frame/data_frame.rs
@@ -73,5 +73,58 @@ impl DataFrame {
         }
         Ok(())
     }
+
+
+
+
+
+
+
+    fn read_header<R: BufRead>(reader: &mut R, options: &Option<DataFrameReadOptions>) -> io::Result<Option<Vec<String>>> {
+    if let Some(opt) = options {
+        if let Some(hh) = &opt.header {
+            match hh {
+                DataFrameHeaderOption::Fixed(headers) => {
+                    println!("Headers: {:?}", headers);
+                    return Ok(Some(headers.clone()));
+                }
+                DataFrameHeaderOption::Auto => {
+                    let mut header_line = String::new();
+                    reader.read_line(&mut header_line)?;
+                    let headers: Vec<String> = header_line.trim().split(',').map(|s| s.to_string()).collect();
+                    println!("Auto headers: {:?}", headers);
+                    return Ok(Some(headers));
+                }
+            }
+        }
+    }
+    Ok(None)
+}
+
+// Function to read the body
+fn read_body<R: BufRead>(reader: &mut R, skip_first_line: bool) -> io::Result<()> {
+    for (index, line) in reader.lines().enumerate() {
+        if skip_first_line && index == 0 {
+            continue;
+        }
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
+    Ok(())
+}
+
+// Main function to read the CSV
+pub fn read_csv(file_name: &str, options: Option<DataFrameReadOptions>) -> io::Result<Foo> {
+    let file = File::open(file_name)?;
+    let mut reader = BufReader::new(file);
+
+    let headers = read_header(&mut reader, &options)?;
+    let skip_first_line = headers.is_some();
+    read_body(&mut reader, skip_first_line)?;
+
+    Ok(Foo)
+}
 }
     */
