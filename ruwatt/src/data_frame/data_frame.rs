b1222efc8523ae2a1a4e912bb1b805b5272aacc2
eugen.michaels-at-927502379560 <eugen.michaels@hannover-re.com>
2024-07-05 15:12:20 +0200

    data frame start

--- ruwatt/src/data_frame/data_frame.rs
+++ ruwatt/src/data_frame/data_frame.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs::File, io::{BufRead, BufReader}};
 
 pub enum DataType {
     Bool(bool),
@@ -16,15 +16,60 @@ pub struct DataFrame {
     pub columns: Vec<DataColumn>
 }
 
+pub enum DataFrameHeaderOption {
+    Fixed(Vec<String>),
+    Auto
+}
+
+pub struct DataFrameReadOptions {
+    header: Option<DataFrameHeaderOption>
+}   
+
+type Foo = Result<(), Box<dyn Error>>;
+
 impl DataFrame {
-    pub fn save_csv(&self, file_name: &str) -> Result<(), Box<dyn Error>> {
+    pub fn save_csv(&self, file_name: &str) -> Foo {
         //for i in 0..self.row_count() {
 
         //}
         Ok(())
     }
 
-    pub fn read_csv(file_name: &str) {
+    pub fn read_csv(file_name: &str, options: Option<DataFrameReadOptions>) -> Foo {
+        let file = File::open(file_name)?;
+        let reader = BufReader::new(file);
+        let headers = Self::read_header(&mut reader, &options)?;
+        Self::read_body(&mut reader, headers)?;
+    }
 
+    fn read_header<R: BufRead>(reader: &mut R, options: &Option<DataFrameReadOptions>) -> io::Result<Option<Vec<String>>> {
+        if let Some(opt) = options {
+            if let Some(hh) = &opt.header {
+                match hh {
+                    DataFrameHeaderOption::Fixed(headers) => {
+                        println!("Headers: {:?}", headers);
+                        return Ok(Some(headers.clone()));
+                    }
+                    DataFrameHeaderOption::Auto => {
+                        let mut header_line = String::new();
+                        reader.read_line(&mut header_line)?;
+                        let headers: Vec<String> = header_line.trim().split(',').map(|s| s.to_string()).collect();
+                        println!("Auto headers: {:?}", headers);
+                        return Ok(Some(headers));
+                    }
+                }
+            }
+        }
+        Ok(None)
+    }
+
+    fn read_body<R: BufRead>(reader: &mut R, headers: Option<Vec<String>>) -> io::Result<()> {
+        for line in reader.lines() {
+            match line {
+                Ok(line) => println!("{}", line),
+                Err(e) => eprintln!("Error reading line: {}", e),
+            }
+        }
+        Ok(())
     }
 }
