c1ffb5601451e03b2b2460ca211df2ef84dd7c3a
eugen.michaels-at-927502379560 <eugen.michaels@hannover-re.com>
2024-07-05 08:55:22 +0200

    determinant

--- ruwatt/src/optimization/systemle.rs
+++ ruwatt/src/optimization/systemle.rs
@@ -3,6 +3,45 @@ use std::iter::Sum;
 use crate::{ assert_ket, assert_square_matrix };
 use crate::tensor::{ dot::dot, Matrix, Tensor };
 
+fn complement<T>(matrix: &Tensor<T>, row_index: usize, col_index: usize) -> Tensor<T> where T: Float {
+    let mut data: Vec<T> = Vec::with_capacity((matrix.row_count() - 1) * (matrix.col_count() - 1));
+
+    (0..matrix.row_count())
+        .filter(|&i| i != row_index)
+        .for_each(|i| {
+            (0..matrix.col_count())
+                .filter(|&j| j != col_index)
+                .for_each(
+                    |j| data.push(matrix.data[i * matrix.col_count() + j])
+                );
+        });
+
+    Tensor {
+        shape: vec![matrix.row_count() - 1, matrix.col_count() - 1],
+        data
+    }
+} 
+
+fn determinant<T>(matrix: &Tensor<T>) -> T where T: Float + Sum {
+    assert_square_matrix!(matrix);
+    if matrix.col_count() == 2 && matrix.row_count() == 2 {
+        matrix.data[0] * matrix.data[3] - matrix.data[1] * matrix.data[2]
+    } else {
+        matrix.row(0).unwrap().data.iter().enumerate()
+            .map(|(index, &value)| {
+                let sign = if index % 2 == 0 { T::one() } else { -T::one() };
+                sign * value * determinant(&complement(matrix, 0, index))
+            })
+            .sum()
+    }
+}
+
+/*
+fn inverse<T>(matrix: &Tensor<T>) -> Tensor<T> {
+    ...?
+}
+    */
+
 // x_(k+1) = (I-A)*x_k + b
 #[allow(dead_code)]
 pub fn system_le<T>(a: &Tensor<T>, b: &Tensor<T>, step_count: usize, delta: T) -> Tensor<T> where T: Float + Sum {
@@ -23,7 +62,25 @@ pub fn system_le<T>(a: &Tensor<T>, b: &Tensor<T>, step_count: usize, delta: T) -
 
 #[cfg(test)]
 mod tests {
-    //use super::{Tensor, system_le};
+    use super::{complement, determinant, Matrix, Tensor};
+
+    fn matrix123() -> Tensor {
+        let data = (1..=9).map(|x| x as f32).collect();
+        return Matrix::square(data);
+    }
+
+    #[test]
+    fn test_complement() {
+        let recieved = complement(&matrix123(), 0, 0);
+        let expected = Matrix::square(vec![5.0, 6.0, 8.0, 9.0]);
+        assert_eq!(recieved, expected)
+    }
+
+    #[test]
+    fn test_determinant() {
+        let recieved = determinant(&matrix123());
+        assert_eq!(recieved, 0.0)
+    }
 
     #[test]
     fn test_system_le() {
